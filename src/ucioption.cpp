8abb824f8ce8f78def46c664a7e3a590e3842874
Marco Zerbinati <zerbinati.marco@gmail.com>
2018-12-19 09:00:50 +0100

    Restore default value

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -96,7 +96,7 @@ void init(OptionsMap& o) {
   o["Syzygy50MoveRule"]      << Option(true);
   o["SyzygyProbeLimit"]      << Option(7, 0, 7);
   o["Dynamic Strategy"]      << Option(true);
-  o["ICCF Analyzes"]         << Option(2, 0,  8);
+  o["ICCF Analyzes"]         << Option(0, 0,  8);
   o["Clear Search"]          << Option(false);
   o["NullMove"]              << Option(true);
   o["LMR"]                   << Option(true);
