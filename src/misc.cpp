56493289d8d11cefc08752343a5476b27ae3b7ac
Marco Zerbinati <zerbinati.marco@gmail.com>
2019-01-08 15:35:59 +0100

    ICCF 080119

--- src/misc.cpp
+++ src/misc.cpp
@@ -127,7 +127,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "ICCF PrO " << Version << setfill('0');
+  ss << "ICCF " << Version << setfill('0');
 
   if (Version.empty())
   {
